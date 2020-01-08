2816e2c9234c3966e313ac6322e5c047e29cf452
agnikuz262 <aga.kuznia@interia.pl>
2020-01-08 10:30:22 +0100

    add about activity

--- app/src/main/java/com/example/cookwithpuppy/MainActivity.kt
+++ app/src/main/java/com/example/cookwithpuppy/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.example.cookwithpuppy
 
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
@@ -13,6 +13,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.cookwithpuppy.ui.AboutActivity
 import com.example.cookwithpuppy.ui.add_recipe.AddRecipeActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.navigation.NavigationView
@@ -39,11 +40,6 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-
-
-            /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show() */
-
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
@@ -64,6 +60,19 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_about -> {
+                val intent = Intent(this, AboutActivity::class.java).apply {
+                }
+                this.startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
---
+++ app/src/main/java/com/example/cookwithpuppy/ui/AboutActivity.kt
@@ -0,0 +1,13 @@
+package com.example.cookwithpuppy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cookwithpuppy.R
+
+class AboutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
