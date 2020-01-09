bcf807f1021e5e6ca6e8fe5fbd9b873390eff431
agnikuz262 <aga.kuznia@interia.pl>
2020-01-09 10:36:22 +0100

    recipe class changes

---
+++ app/src/main/java/com/example/cookwithpuppy/database/Recipe.kt
@@ -0,0 +1,19 @@
+package com.example.cookwithpuppy.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes_table")
+data class Recipe(
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    @ColumnInfo(name="recipe_title")
+    var title: String = "t",
+
+    @ColumnInfo(name="recipe_ingredients")
+    var ingredients: MutableList<String>
+
+)
